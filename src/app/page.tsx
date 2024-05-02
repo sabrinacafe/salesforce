0a7df20eb3a807ccdd041618b3e96fca926d7b21
Sabrina da Motta Café <sabis476@gmail.com>
2024-05-02 15:10:19 -0300

    imagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Home from "./components/Home";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Home />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
