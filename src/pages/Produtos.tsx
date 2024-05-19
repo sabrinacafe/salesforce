15c84242564705c95d1af232568f0633016e032b
Sabrina da Motta Café <sabis476@gmail.com>
2024-05-19 02:30:29 -0300

    teste

---
+++ src/pages/Produtos.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Opcoes from '../app/components/OpcoesProdutos/OpcoesProdutos';
+
+
+const Produtos = () => {
+  return (
+    <div>
+        <Opcoes/>
+    </div>
+  )
+}
+
+export default Produtos;
